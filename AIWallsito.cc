b13b8e44ca9cabb0b39083abe2139e7c54f72f77
sergiowalls <sergio.paredes25@gmail.com>
2016-05-20 22:20:52 +0200

    Renamed to Wallsito

--- AIWallsito.cc
+++ AIWallsito.cc
@@ -6,7 +6,7 @@ using namespace std;
  * Write the name of your player and save this file
  * with the same name and .cc extension.
  */
-#define PLAYER_NAME Wallsi
+#define PLAYER_NAME Wallsito
 
 struct PLAYER_NAME : public Player {
 
